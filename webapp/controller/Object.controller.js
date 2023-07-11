7052e4ee1dbc6026b3ded8c56994468f42dcc9c3
Tiago Castro <tiago.castro@colormaq.com.br>
2023-07-11 01:53:58 +0000

    aula 20230710_01

--- webapp/controller/Object.controller.js
+++ webapp/controller/Object.controller.js
@@ -2,8 +2,9 @@ sap.ui.define([
     "./BaseController",
     "sap/ui/model/json/JSONModel",
     "sap/ui/core/routing/History",
-    "../model/formatter"
-], function (BaseController, JSONModel, History, formatter) {
+    "../model/formatter",
+    "sap/m/MessageBox"
+], function (BaseController, JSONModel, History, formatter, MessageBox) {
     "use strict";
 
     return BaseController.extend("bpmaint25.bpmaint25.controller.Object", {
@@ -24,10 +25,13 @@ sap.ui.define([
             // between the busy indication for loading the view's meta data
             var oViewModel = new JSONModel({
                     busy : true,
-                    delay : 0
+                    delay : 0,
+                    edit : false
                 });
             this.getRouter().getRoute("object").attachPatternMatched(this._onObjectMatched, this);
             this.setModel(oViewModel, "objectView");
+            
+            this.carregaTipo();
         },
         /* =========================================================== */
         /* event handlers                                              */
@@ -49,11 +53,55 @@ sap.ui.define([
                 this.getRouter().navTo("worklist", {}, true);
             }
         },
+        onEditPress: function(){
+            this._changeEditStatus()
+        },
 
+        onSavePress: function () {
+            var that = this;
+            let oModel = this.getOwnerComponent().getModel();
+
+            let oJson = {
+                PartnerId: this.getView().getBindingContext().getObject().PartnerId,
+                //PartnerType: this.byId("txtPartnerType").getValue(),
+                PartnerType: this.byId("cbTipo").getSelectedKey(),
+                PartnerName1: this.byId("txtPartnerName1").getValue(),
+                PartnerName2: this.byId("txtPartnerName2").getValue(),
+                SearchTerm1: this.byId("txtSearchTerm1").getValue(),
+                SearchTerm2: this.byId("txtSearchTerm2").getValue(),
+                Street: this.byId("txtStreet").getValue(),
+                HouseNumber: this.byId("txtHouseNumber").getValue(),
+                District: this.byId("txtDistrict").getValue(),
+                City: this.byId("txtCity").getValue(),
+                Region: this.byId("txtRegion").getValue(),
+                ZipCode: this.byId("txtZipCode").getValue(),
+                Country: this.byId("txtCountry").getValue()
+            }
+
+            oModel.update("/BusinessPartnerSet('" + oJson.PartnerId + "')", oJson, {
+                success: (oData) => {
+                    MessageBox.success(that.getText("msgBPUpdated"), {
+                        title: that.getText("txtBPUpdated"),
+                        onClose: function () {
+                            that._onNavBack(undefined);
+                        }
+                    });
+                },
+                error: (e) => {
+                    MessageBox.error(that.getText("msgBPUpdError"), {
+                        title: that.getText("txtBPUpdError")
+                    });
+                }
+            });
+
+        },
+        onCancelPress: function(){
+                        alert("teste botão cancelar")
+        },
         /* =========================================================== */
         /* internal methods                                            */
         /* =========================================================== */
-
+ 
         /**
          * Binds the view to the object path.
          * @function
@@ -109,6 +157,21 @@ sap.ui.define([
                     oResourceBundle.getText("shareSendEmailObjectSubject", [sObjectId]));
                 oViewModel.setProperty("/shareSendEmailMessage",
                     oResourceBundle.getText("shareSendEmailObjectMessage", [sObjectName, sObjectId, location.href]));
+        },
+        carregaTipo: function () {            let cbTipo = this.byId("cbTipo");
+            cbTipo.addItem(new sap.ui.core.Item({
+                key: 1,
+                text: this.getResourceBundle().getText("txtOrganization")
+            }));
+            cbTipo.addItem(new sap.ui.core.Item({
+                key: 2,
+                text: this.getResourceBundle().getText("txtPerson")
+            }));
+        },
+        _changeEditStatus: function () {
+            let oViewModel = this.getModel("objectView");
+            let bEdit = oViewModel.getProperty("/edit");
+            oViewModel.setProperty("/edit", !bEdit);
         }
     });
 
